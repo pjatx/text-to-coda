8ea97d3e3ce66e68ae7456c1e1e6ff1911110829
Philip Johnson <johnson.philip.louis@gmail.com>
2022-06-30 18:29:59 -0500

    added formatting

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const parseText = (text) => {
   const parsedText = {
     taskType: textArray[0].trim(),
     taskTime: textArray[1].trim(),
-    taskText: textArray[2].trim()
+    taskText: textArray[2].trim(),
   }
 
   return parsedText
@@ -25,7 +25,7 @@ const parseText = (text) => {
 const determineTaskType = (taskTypes, searchString) => {
   const options = {
     includeScore: true,
-    keys: ['taskTypes.name']
+    keys: ['taskTypes.name'],
   }
 
   const fuse = new Fuse(taskTypes, options)
@@ -42,7 +42,7 @@ const returnTaskTypes = async (env) => {
   const url = `${codaEP}/docs/${docId}/tables/${typesTableId}/rows`
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${env.CODA_API_KEY}`
+    Authorization: `Bearer ${env.CODA_API_KEY}`,
   }
 
   const init = {
@@ -51,44 +51,52 @@ const returnTaskTypes = async (env) => {
   }
 
   try {
-    const response = await fetch(url, init);
+    const response = await fetch(url, init)
     const rj = await response.json()
     return rj
   } catch (e) {
     console.log(e)
-    return new Response("Oops! Something went wrong. Please try again later.")
+    return new Response('Oops! Something went wrong. Please try again later.')
   }
 }
 
 const generateCodaData = async (message, env) => {
   const simple = !message.includes(delimiter)
 
-  let data = {rows: [{cells: []}]};
+  let data = { rows: [{ cells: [] }] }
 
   function Cell(columnName, column, value) {
     this.column = column
     this.value = value
   }
 
-  if(simple) {
-    data.rows[0].cells.push(new Cell("Task Name", "c-70z9tdOF3c", message))
-    data.rows[0].cells.push(new Cell("Task Status", "c-kN87N8b6Gr", "Backlog"))
-    data.rows[0].cells.push(new Cell("Needs Triage", "c-2alHSrothg", true))
+  if (simple) {
+    data.rows[0].cells.push(new Cell('Task Name', 'c-70z9tdOF3c', message))
+    data.rows[0].cells.push(new Cell('Task Status', 'c-kN87N8b6Gr', 'Backlog'))
+    data.rows[0].cells.push(new Cell('Needs Triage', 'c-2alHSrothg', true))
   } else {
     const taskTypeTable = await returnTaskTypes(env)
-    const taskTypes = taskTypeTable.items.map(item => item.name)
+    const taskTypes = taskTypeTable.items.map((item) => item.name)
     const parsedText = parseText(message)
     const taskTypeMatch = determineTaskType(taskTypes, parsedText.taskType)
-  
+
     if (!taskTypeMatch) {
-      return console.error("Sorry, I don't know that task type. Please try again.")
+      return console.error(
+        "Sorry, I don't know that task type. Please try again."
+      )
     }
 
-    data.rows[0].cells.push(new Cell("Task Name", "c-70z9tdOF3c", parsedText.taskText))
-    data.rows[0].cells.push(new Cell("Task Status", "c-kN87N8b6Gr", "Backlog"))
-    data.rows[0].cells.push(new Cell("Task Type", "c-eDVIqu2xj_", taskTypeMatch))
-    data.rows[0].cells.push(new Cell("Predicted Duration", "c-L4lltHxi-h", parsedText.taskTime, ))
-    data.rows[0].cells.push(new Cell("Needs Triage", "c-2alHSrothg", true))
+    data.rows[0].cells.push(
+      new Cell('Task Name', 'c-70z9tdOF3c', parsedText.taskText)
+    )
+    data.rows[0].cells.push(new Cell('Task Status', 'c-kN87N8b6Gr', 'Backlog'))
+    data.rows[0].cells.push(
+      new Cell('Task Type', 'c-eDVIqu2xj_', taskTypeMatch)
+    )
+    data.rows[0].cells.push(
+      new Cell('Predicted Duration', 'c-L4lltHxi-h', parsedText.taskTime)
+    )
+    data.rows[0].cells.push(new Cell('Needs Triage', 'c-2alHSrothg', true))
   }
   return data
 }
@@ -104,7 +112,7 @@ const addCodaTodo = async (message, env) => {
   const url = `${codaEP}/docs/${docId}/tables/${taskTableId}/rows`
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + CODA_API_KEY
+    Authorization: 'Bearer ' + CODA_API_KEY,
   }
 
   const init = {
@@ -114,46 +122,46 @@ const addCodaTodo = async (message, env) => {
   }
 
   try {
-    const response = await fetch(url, init);
+    const response = await fetch(url, init)
     const rj = await response.json()
     return rj
   } catch (e) {
     console.log(e)
-    return new Response("Oops! Something went wrong. Please try again later.")
+    return new Response('Oops! Something went wrong. Please try again later.')
   }
 }
 
 export default {
   async fetch(request, env) {
     const OUTBOUND_PHONE = env.OUTBOUND_PHONE
-    
+
     if (request.method != 'POST') {
-      return new Response("Method Not Allowed", {
-        status: 405
+      return new Response('Method Not Allowed', {
+        status: 405,
       })
     }
 
     // Get body of the request - will be text since it's URL encoded
-    const data = await request.text();
+    const data = await request.text()
 
     // Decode URL
-    const params = new URLSearchParams(data);
+    const params = new URLSearchParams(data)
     // Get query params and put in a JS object
-    const twilioObject = Object.fromEntries(params.entries());
-    const fromNumber = twilioObject.From;
+    const twilioObject = Object.fromEntries(params.entries())
+    const fromNumber = twilioObject.From
 
     // Only allow texts from the users number
     if (fromNumber != OUTBOUND_PHONE) {
-      return new Response("Forbidden", {
-        status: 403
+      return new Response('Forbidden', {
+        status: 403,
       })
     }
 
-    const message = twilioObject.Body;
+    const message = twilioObject.Body
 
     const response = await addCodaTodo(message, env)
     console.log(JSON.stringify(response))
 
-    return new Response("Item successfully added!")
+    return new Response('Item successfully added!')
   },
-};
+}
