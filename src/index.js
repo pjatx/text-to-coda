1cfae6c6c7aa88af2be54c7e938e95c586ee5c46
Philip Johnson <johnson.philip.louis@gmail.com>
2023-02-26 16:30:20 -0600

    removed triage column

--- src/index.js
+++ src/index.js
@@ -74,8 +74,8 @@ const generateCodaData = async (message, env) => {
 
   if (simple) {
     data.rows[0].cells.push(new Cell('Task Name', 'c-70z9tdOF3c', message))
-    data.rows[0].cells.push(new Cell('Task Status', 'c-kN87N8b6Gr', 'Backlog'))
-    data.rows[0].cells.push(new Cell('Needs Triage', 'c-2alHSrothg', true))
+    data.rows[0].cells.push(new Cell('Task Status', 'c-kN87N8b6Gr', 'Today'))
+    // data.rows[0].cells.push(new Cell('Needs Triage', 'c-2alHSrothg', true))
   } else {
     const taskTypeTable = await returnTaskTypes(env)
     const taskTypes = taskTypeTable.items.map((item) => item.name)
